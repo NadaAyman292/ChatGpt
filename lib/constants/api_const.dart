feec3162efbcea2b4ffa51282b0e6e4d0d263f51
NadaAyman292 <nadaayman0530@gmail.com>
2023-04-17 19:52:57 +0200

    second commit

--- lib/constants/api_const.dart
+++ lib/constants/api_const.dart
@@ -1,3 +1,3 @@
-String apiKey = "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";
+String apiKey = "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";
 
 String baseUrl = "https://api.openai.com/v1";
